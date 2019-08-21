5b2e287972776e8e643abe163c2d076937c46472
Molnar Andras <andris9016@gmail.com>
2019-08-21 17:18:03 +0200

    Add ViewModel class(MVVM design pattern)

---
+++ PingPong/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace PingPong
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private Ball ball = new Ball(240, 200);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyRaised(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public double BallXPos
+        {
+            get { return ball.XPos; }
+            set
+            {
+                ball.XPos = value;
+                OnPropertyRaised("BallXPos");
+            }
+        }
+
+        public double BallYPos
+        {
+            get { return ball.YPos; }
+            set
+            {
+                ball.YPos = value;
+                OnPropertyRaised("BallYPos");
+            }
+        }
+    }
+}
