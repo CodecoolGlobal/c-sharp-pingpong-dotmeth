5764d3a11c83ca73e22abe4756f3625fdea97bce
Molnar Andras <andris9016@gmail.com>
2019-08-21 16:44:52 +0200

    Add ball class with x and y coordinates

---
+++ PingPong/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    class Ball
+    {
+        private double xPos;
+        private double yPos;
+
+        public Ball(double xPos, double yPos)
+        {
+            XPos = xPos;
+            YPos = yPos;
+        }
+
+        public double XPos { get => xPos; set => xPos = value; }
+        public double YPos { get => yPos; set => yPos = value; }
+    }
+}
