6285238c9191718536af311608022061c58475ab
Zsuzsanna Rónai <ronai.zsuzsanna@gmail.com>
2019-08-21 15:01:16 +0200

    move paddle

--- PingPong/MainWindow.xaml.cs
+++ PingPong/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,24 @@ namespace PingPong
         public MainWindow()
         {
             InitializeComponent();
+            KeyDown += new KeyEventHandler(Paddle_Move);
+        }
+
+        public void Paddle_Move(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                //var trs = new TranslateTransform();
+                //var anim3 = new DoubleAnimation(0, -20, TimeSpan.FromSeconds(2));
+                //trs.BeginAnimation(TranslateTransform.XProperty, anim3);
+                //MyPaddle.RenderTransform = trs;
+                Canvas.SetLeft(Paddle, Canvas.GetLeft(Paddle) - 10);
+            }
+            if (e.Key == Key.Right)
+            {
+                Canvas.SetLeft(Paddle, Canvas.GetLeft(Paddle) + 10);
+            }
         }
     }
 }
+
